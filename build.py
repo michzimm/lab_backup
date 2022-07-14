b18875f8d405b265849b2791b3bae8053d405485
michzimm <michzimm@cisco.com>
2022-07-13 22:00:18 -0400

    initial_push

---
+++ build.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+import getpass
+
+passwd = getpass.getpass("Enter password: ")
+version = input("Enter build tag version: ")
+
+command = 'docker build --build-arg passwd='+passwd+' . -t lab_backup:'+version
+os.system(command)
